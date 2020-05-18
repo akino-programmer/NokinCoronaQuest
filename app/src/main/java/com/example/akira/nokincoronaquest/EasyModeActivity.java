cc23519a837f82d5bbd0dbccd5d1eb815b26b48a
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-05-18 23:17:06 +0900

    EasyMode firstView

--- app/src/main/java/com/example/akira/nokincoronaquest/EasyModeActivity.java
+++ app/src/main/java/com/example/akira/nokincoronaquest/EasyModeActivity.java
@@ -4,13 +4,61 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class EasyModeActivity extends AppCompatActivity {
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_mode);
+
+
+//        TextView FirstHp = (TextView) findViewById(R.id.easy_corona_monster_hp);
+//        TextView FirstMenu = (TextView) findViewById(R.id.menu_name);
+//        TextView FirstTimes = (TextView) findViewById(R.id.numberOfTimes);
+
+
+
+        Button start = (Button) findViewById(R.id.start_button);
+        start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                TextView FirstHp = (TextView) findViewById(R.id.easy_corona_monster_hp);
+
+                Random random = new Random();
+                int randomValue = random.nextInt(100);
+
+
+            FirstHp.setText("：" + randomValue + "/" + randomValue);
+
+
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         Button back = (Button) findViewById(R.id.back_button);
         back.setOnClickListener(new View.OnClickListener(){
