55e79ff0d0ee894ebd696051372d8ace44af6048
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-05-18 19:08:12 +0900

    View move to EasyMode.

---
+++ app/src/main/java/com/example/akira/nokincoronaquest/EasyModeActivity.java
@@ -0,0 +1,23 @@
+package com.example.akira.nokincoronaquest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class EasyModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy_mode);
+
+        Button back = (Button) findViewById(R.id.back_button);
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
+    }
+}
